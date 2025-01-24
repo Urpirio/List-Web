cdc1fbaa4523c20663e6b8271f6014317b7581cb
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-01-24 04:37:19 -0400

    Se agrego un data array para iniciar sesion o simular la informacion del usuario.

---
+++ Src/components/Sventana/Data/SvheaderData.js
@@ -0,0 +1,10 @@
+const SvheaderData = [
+    {
+        Photoperfil: "Src/components/Sventana/img/Mental-Strong-Women-min.jpg",
+        descripcionIMG: "Foto de perfil",
+        Username: "Julia martinez",
+        id:"Juliamartinez",
+    },
+    
+]
+export default SvheaderData;
