3af756f7b13f722f8e999ff62a7c868a7a95cae1
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-01-25 18:57:52 -0400

    Se agrego un nuevo componente.

--- Src/components/main/Smain.jsx
+++ Src/components/main/Smain.jsx
@@ -1,22 +1,21 @@
 import React, { Component } from 'react';
 import "./style/Smain.less";
 import SmainText from './SmainText';
+import SmainModalTarea from './SmainModalTarea';
 
 export default class Smain extends Component {
   render() {
 
-    
+     
 
     return (
       <div className='Smain__div1' id='Smain__div1'>
-        <div className='SM_div1_div1'> 
-            <h1>Bandeja de entrada</h1>
-            <span><i class="bi bi-plus"></i>Añadir tarea</span>
-        </div>
+        <SmainModalTarea/>
         <div className='listarea'>
 
         </div>
        <SmainText/>
+       
       </div>
     )
   }
