a8ea79e46514b6fe423b981b9f875242ed802b78
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-01-25 19:05:10 -0400

    Se comenzo a trabajar en diversas funciones.

--- Src/components/Sventana/Svmain.jsx
+++ Src/components/Sventana/Svmain.jsx
@@ -4,10 +4,16 @@ import "./style/Svmain.less";
 
 export default class SVmain extends Component {
   render() {
+
+    const AddHomeTarea = () =>{
+      document.getElementById("SM_div2_div1").style.display = "flex";
+    };
+
+
     return (
       <main className='Svmain'>
         <div className='Svmain__div1'>
-            <span id='Svm__tareas'><i class="bi bi-plus"></i>Añadir tarea</span>
+            <span onClick={AddHomeTarea} id='Svm__tareas'><i class="bi bi-plus"></i>Añadir tarea</span>
             <span id='Svm__Buscador'><i class="bi bi-search"></i>Buscador</span>
             <span id='Svm__BnEntrada'><i class="bi bi-inbox"></i>Bandeja de entrada</span>
         </div>
